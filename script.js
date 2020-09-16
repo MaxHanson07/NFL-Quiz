a2c53b9ef69dfeeb18201726e5bfdf6a94c04e16
Max Hanson <mthanson2@gmail.com>
2020-09-15 18:21:03 -0700

    Added functions that keep track of user's answers and checks if they are right. Also keeps track of score and subtracts time if answer is wrong.

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+// Create variables needed for clock
+// var timeEl = document.querySelector(".time");
+var secondsLeft = 2;
+var gameOver = false;
+
+// Timer that counts down from 30 seconds
+function countdownTimer() {
+    // Create the countdown timer.
+
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        document.querySelector(".time").textContent = "Time left: " + secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            gameOver = true;
+            console.log(gameOver);
+        }
+
+    }, 1000);
+
+}
 
 // <!-- Start Button that starts a timer and displays a question when clicked  -->
-
 var startButton = document.getElementById("start");
-
-startButton.addEventListener("click", function () {
-    countdownTimer();
-    questionTime();
-})
-
-// Replaces original content with header = question number, body = question, button = answered AND * 4
 
 var header = document.querySelector(".header");
 var body = document.querySelector(".body");
@@ -19,63 +33,74 @@ var answer2 = document.createElement("button");
 var answer3 = document.createElement("button");
 var answer4 = document.createElement("button");
 
+startButton.addEventListener("click", function () {
+    countdownTimer();
+    // askQuestion();
+})
+
+
+
 
 
 // Variables that keep track of question number and score
 var questionNum = 1;
+var element = 0;
 var score = 0;
 // Variable used for iterating over array
-var element = questionNum - 1;
+
 
 // The array of questions for the NFL quiz
 var questions = [
     { q: "The sky is blue.", a: "t", b: "jo", c: "jo", d: "jo", r: "b" },
-    { q: "There are 365 days in a year.", a: "t", b: "jo", c: "jo", d: "jo" },
-    { q: "There are 42 ounces in a pound.", a: "t", b: "jo", c: "jo", d: "jo" },
-    { q: "The Declaration of Independence was created in 1745.", a: "t", b: "jo", c: "jo", d: "jo" },
-    { q: "Bananas are vegetables.", a: "t", b: "jo", c: "jo", d: "jo" }
+    { q: "There are 365 days in a year.", a: "t", b: "jo", c: "jo", d: "jo", r: "c" },
+    { q: "There are 42 ounces in a pound.", a: "t", b: "jo", c: "jo", d: "jo", r: "b" },
+    { q: "The Declaration of Independence was created in 1745.", a: "t", b: "jo", c: "jo", d: "jo", r: "a" },
+    { q: "Bananas are vegetables.", a: "t", b: "jo", c: "jo", d: "jo", r: "c" }
 ];
 
+// Asks user questions until all questions are answered or time runs out
+function askQuestion() {
+    while ((element < questions.length) && (gameOver != true)) {
+        questionDisplay();
+    }
+}
+
 // Displays questions and answers
-function questionTime(element) {
+function questionDisplay() {
 
-    // header.textContent = "Question " + questionNum;
-    // body.textContent = questions[element].q;
-    // answer1.textContent = questions[element].a;
-    // answer2.textContent = questions[element].b;
-    // answer3.textContent = questions[element].c;
-    // answer4.textContent = questions[element].d;
+    header.textContent = "Question " + questionNum;
+    body.textContent = questions[element].q;
+    answer1.textContent = questions[element].a;
+    answer2.textContent = questions[element].b;
+    answer3.textContent = questions[element].c;
+    answer4.textContent = questions[element].d;
 
-    answer1.addEventListener("click", function() {
-        if (questions[element].r === "1")
-        {
+    checkAnswer(answer1, a);
+    checkAnswer(answer2, b);
+    checkAnswer(answer3, c);
+    checkAnswer(answer4, d);
+
+    body.appendChild(answer1);
+    body.appendChild(answer2);
+    body.appendChild(answer3);
+    body.appendChild(answer4);
+
+}
+
+console.log(questions[element].a);
+
+
+function checkAnswer(selectedAnswer, value) {
+    selectedAnswer.addEventListener("click", function () {
+        if (questions[element].r === value) {
             score++;
         }
         else {
             secondsLeft = secondsLeft - 10;
         }
         questionNum++;
+        element++;
     })
-
-    // body.appendChild(answer1);
-    // body.appendChild(answer2);
-    // body.appendChild(answer3);
-    // body.appendChild(answer4);
-
-
-    // Give on click values to buttons
-
-
-    // Check if correct
-    if ()
-
-
-
-    
-
-
-
-
 }
 
 // Append buttons with additional answers
@@ -98,57 +123,4 @@ function questionTime(element) {
 
 // Cycle through array displaying new question
 
-
-
-
-
-
-// Create variables needed for clock
-var timeEl = document.querySelector(".time");
-var secondsLeft = 2;
-var gameOver = false;
-
-// Timer that counts down from 30 seconds
-function countdownTimer() {
-    // Create the countdown timer.
-    gameOver = false;
-
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        //   timeEl.textContent = "Time left: " + secondsLeft;
-
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            gameOver = true;
-        }
-
-    }, 1000);
-
-
-}
-
-//   function speedRead() {
-//     // Print words to the screen one at a time.
-//     timeEl.textContent = " ";
-
-
-
-//     var reader = document.createElement("h2");
-
-//     var i = 0;
-
-//     var timerInterval2 = setInterval(function() {
-
-
-
-//       reader.textContent = peomArr[i];
-
-//       i++;
-
-//       mainEl.appendChild(reader);
-
-//     }, 500)};
-
-countdownTimer();
-
-console.log(gameOver);
+// Need to create highscore js and local storage
